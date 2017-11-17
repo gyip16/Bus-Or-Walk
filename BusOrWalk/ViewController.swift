186043398c9437bcd2687075a0566f19c4ca0d17
Gabriel Yip <gyip16@my.bcit.ca>
2017-11-17 15:08:41 -0800

    Initial Commit

---
+++ BusOrWalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusOrWalk
+//
+//  Created by Gabriel Yip on 2017-11-17.
+//  Copyright © 2017 Gabriel Yip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
